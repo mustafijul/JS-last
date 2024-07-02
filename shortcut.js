31c48f5a9c5da37eca4b5342fbb2c503c194c207
mustafij <mustafijulislam21@gmail.com>
2024-07-02 10:18:32 +0600

    explored some shortcut system of js for conversion

---
+++ shortcut.js
@@ -0,0 +1,16 @@
+// ternary operator
+let money = 100;
+let food = money ? 'biriyani' : 'chaBiscuit';
+// console.log(food);
+
+// conversion shortcut
+
+const name = '105';
+const numberName = +name;
+// console.log(numberName);
+
+const stringNum = 2024;
+const stringNumConvert = stringNum + '';
+console.log(stringNumConvert);
+
+
